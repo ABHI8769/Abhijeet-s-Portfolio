561a060b37c80bc5e48f33f4e8db1eb818edfc5a
unknown <abhijeet.goswami78@gmail.com>
2024-05-10 22:01:48 +0530

    Abhijeet's-Portfolio

--- src/container/footer/Footer.jsx
+++ src/container/footer/Footer.jsx
@@ -2,7 +2,7 @@ import './Footer.css'
 
 const Footer = () => {
   return <div className='app__footer'>
-    <a href='https://github.com/nxxwxj/portfolio-website'>&lt; Designed and Built by AJ /&gt;</a>
+    <a>&lt; Designed and Built by AJ /&gt;</a>
   </div>;
 };
 
