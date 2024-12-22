7245299f39a68f705685d2e91e46c98267ccd0d0
unknown <abhijeet.goswami78@gmail.com>
2024-12-22 22:27:20 +0530

    Abhijeet's protfolio

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -14,54 +14,71 @@ function Experience() {
           <h2>Software Developer Intern</h2><h4>June 2024 – Present </h4>
           <p>LG Soft Ltd.<br/>Bangalore , IN</p>
           <ul>
-            <li>
+          <li>
               <span>
                 <BsFillCaretRightFill />
               </span>
-              <span className="tech__heading">VOIP and VOLTE Development: </span>
               <span className="tech__name">
-                Working on the development and optimization of Voice
-                over IP (VOIP) and Voice over LTE (VOLTE) applications using Core C++ Concepts.
+                Developed a project individually for the automation of Autosar log analysis.
               </span>
             </li>
             <li>
               <span>
                 <BsFillCaretRightFill />
               </span>
-              <span className="tech__heading">Software Development: </span>
+              <span className="tech__heading">Automated Log Analysis: </span>
               <span className="tech__name">
-                Contributing to the development and maintenance of software,
-                ensuring seamless communication and data transfer.
+              Designed and developed a log analyzer tool to automate the analysis of Autosar DLT logs, significantly improving the
+              efficiency of log processing for the development team.
               </span>
             </li>
             <li>
               <span>
                 <BsFillCaretRightFill />
               </span>
-              <span className="tech__heading">Testing & Debugging: </span>
+              <span className="tech__heading">UI Representation: </span>
               <span className="tech__name">
-                Assisting in testing and debugging VOIP and VOLTE software
-                applications, identifying and resolving issues to improve performance and reliability.
+              Implemented advanced filtering functionalities to allow developers to sift through logs more effectively. The tool visually
+              represents the logs, making it easier to identify patterns, track issues, and debug crashes.
               </span>
             </li>
             <li>
               <span>
                 <BsFillCaretRightFill />
               </span>
-              <span className="tech__heading">Collaboration: </span>
+              <span className="tech__heading">Performance Optimization: </span>
               <span className="tech__name">
-                Collaborating with cross-functional teams to integrate connectivity and
-                communication solutions.
+              Focused on optimizing the tool's performance to handle large-scale logs efficiently, ensuring smooth user interaction
+              and quick data processing.
               </span>
             </li>
             <li>
               <span>
                 <BsFillCaretRightFill />
               </span>
-              <span className="tech__heading">Project Management: </span>
+              <span className="tech__heading">Integration with Development Workflow: </span>
               <span className="tech__name">
-                Attending daily stand-ups and sprint planning sessions to ensure
-                timely project completion.
+              Integrated the log analyzer seamlessly into the existing development workflow, reducing manual effort
+              and minimizing the time spent on debugging the logs.
+              </span>
+            </li>
+            <li>
+              <span>
+                <BsFillCaretRightFill />
+              </span>
+              <span className="tech__heading">Collaboration and Feedback: </span>
+              <span className="tech__name">
+              Worked closely with cross-functional teams, gathering feedback from developers to iteratively improve the tool's
+              usability and effectiveness.
+              </span>
+            </li>
+            <li>
+              <span>
+                <BsFillCaretRightFill />
+              </span>
+              <span className="tech__heading">Tools & Technologies: </span>
+              <span className="tech__name">
+              Python, QT for UI development
               </span>
             </li>
           </ul>
